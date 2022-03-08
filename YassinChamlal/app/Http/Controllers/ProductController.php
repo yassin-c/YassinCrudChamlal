269ce87f517b918873389ba0c79935e5edd1640e
Yassin Chamlal <81307380+yassin-c@users.noreply.github.com>
2022-03-08 12:07:17 +0100

    index functie aangevuld

--- YassinChamlal/app/Http/Controllers/ProductController.php
+++ YassinChamlal/app/Http/Controllers/ProductController.php
@@ -14,7 +14,11 @@ class ProductController extends Controller
      */
     public function index()
     {
-        //
+        $products = Product::latest()->paginate(5);
+
+        return view('products.index',compact('products'))
+
+            ->with('i', (request()->input('page', 1) - 1) * 5);
     }
 
     /**
