48a3ca86182c8decef3aec403a0ba115e83183d0
Yassin Chamlal <81307380+yassin-c@users.noreply.github.com>
2022-03-08 12:19:28 +0100

    Navigatie aangepast

--- YassinChamlal/resources/views/layouts/navigation.blade.php
+++ YassinChamlal/resources/views/layouts/navigation.blade.php
@@ -14,6 +14,9 @@
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
+                    </x-nav-link> 
+                    <x-nav-link :href="route('products.index')" :active="request()->routeIs('products.index')">
+                        {{ __('Berichten') }}
                     </x-nav-link>
                 </div>
             </div>
