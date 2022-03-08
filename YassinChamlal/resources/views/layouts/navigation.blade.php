0400a492da5a9fb88c684dc54e75280b157f439f
Yassin Chamlal <81307380+yassin-c@users.noreply.github.com>
2022-03-08 13:52:12 +0100

    menu voor mobile

--- YassinChamlal/resources/views/layouts/navigation.blade.php
+++ YassinChamlal/resources/views/layouts/navigation.blade.php
@@ -68,6 +68,9 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link> 
+             <x-responsive-nav-link :href="route('products.index')" :active="request()->routeIs('products.index')">
+                {{ __('Berichten') }}
             </x-responsive-nav-link>
         </div>
 
